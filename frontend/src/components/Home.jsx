f713fbd2f22d6dd6e70d41c9dd9fb2fa6e3d91b6
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-13 20:47:08 +0700

    fetch data

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import { useGetAllProductsQuery } from '../features/productsApi';
+// import { useSelector } from 'react-redux';
+import { productsApi, useGetAllProductsQuery } from '../features/productsApi';
 
 const Home = () => {
+  // const { items, status } = useSelector(state => state.products)
   const { data, error, isLoading } = useGetAllProductsQuery();
   return (
-    <div>Home</div>
+    <div>
+      { isLoading ? (<p>Loading...</p>) : error ? (<p>An error occured...</p>) : (
+        <>
+        <h2>Limited Edition</h2>
+        <div>
+          {data?.map((product) => {
+            return (
+              <div key={product.id}>
+                <h3>{productsApi.name}</h3>
+                <img src={product.image} alt={product.name}/>
+                <p>{product.price}</p>
+                <p>{product.desc}</p>
+                <button>Add To Cart</button>
+              </div>
+            )
+          })}
+        </div>
+        </>
+      )}
+    </div>
   )
 }
 
