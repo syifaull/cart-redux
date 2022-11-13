981241d37514cf61d686f93e54fef3f9ad03377a
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-13 14:57:04 +0700

    add products API

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const products = require('./products')
+
 const app = express();
 
 app.use(express.json());
@@ -11,7 +13,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.send([2, 3, 4]);
+  res.send(products);
 })
 
 const port = process.env.PORT || 5000
---
+++ backend/products.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    id: 1,
+    name: 'Pleated Bag',
+    desc: 'Pink and cute',
+    price: 5000000,
+    image: 'https://cdn-images.farfetch-contents.com/18/37/77/66/18377766_39500797_300.jpg',
+  },
+  {
+    id: 2,
+    name: 'Bautelo Bag',
+    desc: 'Beautiful bag',
+    price: 3500000,
+    image: 'https://cdn-images.farfetch-contents.com/16/57/84/27/16578427_32360078_300.jpg',
+  },
+  {
+    id: 3,
+    name: 'Chain Bag',
+    desc: 'Black and cute',
+    price: 2900000,
+    image: 'https://cdn-images.farfetch-contents.com/17/68/20/75/17682075_37090614_300.jpg',
+  },
+  {
+    id: 4,
+    name: 'Pleated Bag',
+    desc: 'White and cute',
+    price: 5000000,
+    image: 'https://cdn-images.farfetch-contents.com/18/46/28/99/18462899_39599902_300.jpg',
+  },
+  {
+    id: 5,
+    name: 'Pleated Bag',
+    desc: 'Pink and cute',
+    price: 5000000,
+    image: 'https://cdn-images.farfetch-contents.com/18/37/77/66/18377766_39500797_300.jpg',
+  },
+  {
+    id: 6,
+    name: 'Grin Bag',
+    desc: 'Black and luxury',
+    price: 9000000,
+    image: 'https://cdn-images.farfetch-contents.com/19/01/37/25/19013725_41435952_300.jpg',
+  },
+];
+
+module.exports = products;
