f7b0c5682df302779fce2b6d438c3fccc32465b7
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-15 21:56:15 +0700

    add popup and localStorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 import Cart from './components/Cart';
 import Home from './components/Home';
@@ -10,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <ToastContainer/>
       <NavbarComp/>
         <Routes>
           <Route exact path='/' element = {<Home/>} />
--- frontend/src/features/CartSlice.js
+++ frontend/src/features/CartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
-  cartItems: [],
+  cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
   cartTotalQuantity: 0,
   cartTotalAmount: 0,
 }
@@ -14,10 +15,18 @@ const cartSlice = createSlice({
       const itemIndex = state.cartItems.findIndex((item) => item.id === action.payload.id);
       if (itemIndex >= 0) {
         state.cartItems[itemIndex].cartQuantity += 1;
+        toast.info(`increase ${state.cartItems[itemIndex].name} cart quantity`, {
+          position: "bottom-right"
+        });
       } else {
         const tempProduct = { ...action.payload, cartQuantity: 1}
         state.cartItems.push(tempProduct);
+        toast.success(`added ${action.payload.name} to cart`, {
+          position: "bottom-right"
+        });
       }
+
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     }
   }
 });
