ecd305c92eac49d55e5fca2e9a7024038735176c
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-21 09:28:42 +0700

    add cart quantity feature

--- frontend/src/components/NavbarComp.jsx
+++ frontend/src/components/NavbarComp.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const NavbarComp = () => {
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
+
   return (
     <div>
       <Navbar bg="light" variant="light">
-        <Container >
+        <Container>
           <Navbar.Brand href="/">ONLINE SHOP</Navbar.Brand>
           <Nav>
             <Nav.Link href="/cart">
               <i className="bi bi-basket3-fill fs-3 nav-cart"></i>
               <span className="cart-quantity fs-6 ">
-              <span>5</span>
+                <span>{cartTotalQuantity}</span>
               </span>
             </Nav.Link>
           </Nav>
